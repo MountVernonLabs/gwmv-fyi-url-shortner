0590e17adc211db77444f037bcfccca234adaa8a
Matthew Briney <mbriney@mb-macbook.local>
2020-01-15 14:55:28 -0500

    Copy to GitHub

---
+++ user/plugins/aiaraldea-qr-google-charts/inline-qrcode.js
@@ -0,0 +1,18 @@
+function qrcode_code() {
+  var url = $( '#copylink' ).attr('value');
+  var qrcimg = 'http://chart.apis.google.com/chart?chs=150x150&cht=qr&chld=M&chl='+url;
+  var qrcimglnk = 'http://chart.apis.google.com/chart?chs=500x500&cht=qr&chld=M&chl='+url;	
+  var insertimg = "<div id='qrcode2' class='share'><a href="+qrcimglnk+"><img id='myid' src='" + qrcimg + "' /></a></div>"
+
+  if ( $( '#qrcode2' ).length > 0 ) {
+    $( '#qrcode2' ).remove( );
+  }
+  if ($( "#shareboxes" )) {  
+    $( "#shareboxes" ).append( insertimg );        // Append new elements
+    $( "div#qrcode2 img" ).css( "width", "100px" );
+    $( "div#qrcode2 img" ).css( "height", "100px" );
+  }
+
+}
+
+$(document).ready( function(){ qrcode_code() } );
